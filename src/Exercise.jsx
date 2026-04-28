b2520bb2946f0860be8ef251145bf8129114cd96
AbdKhlm <abdulkhalim0805@gmail.com>
2026-04-28 13:55:08 +0700

    update postcard

--- src/Exercise.jsx
+++ src/Exercise.jsx
@@ -26,7 +26,7 @@ function Exercise() {
           />
         </div>
         <br></br>
-        <h1 className="text-3xl font-bold text-center mb-6 text-blue-700">
+        <h1 className="text-3xl font-bold text-center mb-6 text-specialRed2">
           Post Cards
         </h1>
 
